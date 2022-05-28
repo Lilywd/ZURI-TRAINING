3298d66eda18cedbe40837ab1cc29b749dc77d5d
Lilywd <ndungulilianwanjiku@gmail.com>
2022-05-28 20:26:58 +0300

    Classes and Objects

---
+++ ZURI_Python/classesandobjects.py
@@ -0,0 +1,45 @@
+#class for student
+class Student:
+    
+
+        
+    def __init__(self,name,age,tracks,score):
+        
+        #instance variable
+        self.name = name
+        self.age = int(age)
+        self.tracks = tracks
+        self.score = float(score)
+
+
+    def change_name(self,new_name):
+        self.name =new_name
+        #return self.name
+        print(new_name)
+    
+    def change_age(self, new_age):
+        self.age = int(new_age)
+       #return self.age
+        print(new_age)
+        
+    def add_track(self,new_tracks):
+        self.track = new_tracks
+        #return self.track
+        print(new_tracks)
+
+    def get_score(self,score):
+        #return self.score
+        print(score)
+    
+
+
+#objects for student class
+Bob = Student(name="Bob", age=26, tracks=["FE","BE"],score=20.90)
+        
+
+# Expected methods
+Bob.change_name("Peter")
+Bob.change_age(34)
+Bob.add_track("UI/UX")
+Bob.get_score("2.5")
+
