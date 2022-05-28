44e64ad69a2ba10f5544abb159c6ce203f63bab6
Lilywd <ndungulilianwanjiku@gmail.com>
2022-05-28 16:22:50 +0300

    string anagram detector

---
+++ ZURI_Python/anagram.py
@@ -0,0 +1,26 @@
+# Check if two words are anagrams 
+# Example:
+# find_anagrams("hello", "check") --> False
+# find_anagrams("below", "elbow") --> True
+
+
+
+def find_anagram(word, anagram):
+
+ # the sorted strings are checked
+    if(sorted(str1)== sorted(str2)):
+        print("True")
+    else:
+        print("False")        
+                                          
+# strings to check 
+str1 ="care"
+str2 ="race"
+
+print("string1:",str1)
+print("string2:",str2)
+find_anagram(str1, str2)
+
+  #  return True
+
+
