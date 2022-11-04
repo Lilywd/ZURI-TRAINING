95dba9cae327208176b427992c4ef49e9fcefc54
Lilywd <ndungulilianwanjiku@gmail.com>
2022-11-05 01:14:00 +0300

    FLIIGHT API

---
+++ ZURI_nodejs/Booking-flight-api/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const { json } = require("express");
+const flights = require("./controllers/flightController");
+const models = require("./models/Flight");
+const routes = require("./routes/flightRoute");
+
+const app = express();
+
+app.use(json());
+
+app.use("/", routes);
+ 
+
+
+
+app.get('/', (req, res) => {
+  const flight = {
+    title:  "flight to canada",
+    time: "1pm",
+    price : 26000,
+    date : "26-06-2022"
+  }
+  res.send(flight)
+})
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
